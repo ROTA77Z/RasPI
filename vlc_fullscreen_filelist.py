b6d435ca0010ed8d530909d8ac11e1ee2f86ada0
ROTA77Z <rota77z@hotmail.co.jp>
2021-08-25 00:58:19 +0900

    Update and rename vlc_fullscreen_filelist to vlc_fullscreen_filelist.py

---
+++ vlc_fullscreen_filelist.py
@@ -0,0 +1,18 @@
+# VLC Full screen display with in directory
+# importing vlc module
+import vlc
+# importing time module
+import time
+# creating vlc media player object
+media_player = vlc.MediaPlayer()
+# toggling full screen
+media_player.toggle_fullscreen()
+# media object
+media = vlc.Media("/media/pi/DRIVERS/VIDEO/Daylight.mp4")
+# setting media to the media player
+media_player.set_media(media)
+# start playing video
+media_player.play()
+# wait so the video can be played for 5 seconds
+# irrespective for length of video
+time.sleep(5)
